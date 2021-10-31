068050072e934ed96895c9d4fece256e83e0f18a
unknown <inariwind@gmail.com>
2021-10-31 19:57:06 +0200

    feat: Add TS, npm ci, backend hot reload.

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || port, () =>
+  console.log(
+    "\x1b[36m%s\x1b[33m%s\x1b[0m",
+    "Backend server is listening on port: ",
+    `${process.env.PORT || port}`
+  )
+);
