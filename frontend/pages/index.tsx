55db483c9ad3cd428e75e2debd41b2d863919e1d
O-K-G <inariwind@gmail.com>
2021-11-12 17:03:36 +0200

    feat: Add Ubuntu within Windows instructions.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import classes from "../styles/Home.module.css";
 
-const Home: NextPage = ({ENV}:any) => {
+const Home: NextPage = ({ ENV }: any) => {
   return (
     <>
       <Head>
@@ -13,16 +13,43 @@ const Home: NextPage = ({ENV}:any) => {
       <main>
         <h1 className={classes.titles}>A skeletal Docker\Next.js\Express.js\Mongo project</h1>
       </main>
-    
-    <section>
-      <h3 className={classes.titles}>In the command line:</h3>
-      <p>Write &quot;docker-compose up&quot; when in development.</p>
-      <p>Write &quot;ENV=prod docker-compose up&quot; when in production.</p>
-      <p>Current environment variable: {ENV}</p>
-    </section>
+
+      <section>
+        <h3 className={classes.titles}>In the command line:</h3>
+        <p>Write &quot;docker-compose up&quot; when in development.</p>
+        <p>Write &quot;ENV=prod docker-compose up&quot; when in production.</p>
+        <p>Current environment variable: {ENV}</p>
+      </section>
+
+      <section>
+        <h3 className={classes.titles}>Develop in Ubuntu within Windows:</h3>
+        <p>1. Install WIndows features Windows subsystem for Linux. Virtual Machine Platform.<br></br>
+          2. Update Windows. <br></br>
+          3. Add in Windows' {"Users/<user name/"} the file .wslconfig:<br></br>
+          ---------------<br></br>
+
+          [wsl2]<br></br>
+          memory=8gb<br></br>
+          processors=2<br></br>
+
+          [interop]<br></br>
+          appendWindowsPath = false<br></br>
+
+          --------------- <br></br>
+          4. Install MS store Ubuntu and open it. <br></br>
+          5. Update/upgrade Ubuntu. https://www.cyberciti.biz/faq/upgrade-update-ubuntu-using-terminal/ <br></br>
+          6. Install Node in Linux. https://github.com/nodesource/distributions/blob/master/README.md <br></br>
+          7. Instal Docker + repo. https://docs.docker.com/engine/install/ubuntu/ <br></br>
+          8. Run 'sudo service docker start'<br></br>
+          9. Install Docker Compose V2. https://docs.docker.com/compose/cli-command/#install-on-linux<br></br>
+          10. Manage Docker as a non-root user. https://docs.docker.com/engine/install/linux-postinstall/<br></br>
+          11. Clone a project to a folder within Ubuntu and Docker Compose Up.
+        </p>
+
+      </section>
 
       <footer>
-        
+
       </footer>
     </>
   );
