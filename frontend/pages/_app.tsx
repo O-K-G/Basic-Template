598ae59ea4e3ed4a475d7473e4d3728fb0480c7b
root <root@Main.localdomain>
2021-11-19 14:57:33 +0200

    feat: Content update.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,40 @@
+import Head from "next/head";
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return <><Head>
+    <title>Basic Web App Template</title>
+    <meta charSet="UTF-8" />
+    <meta
+      name="description"
+      content="A quick guide for a Docker \ webapp development environment with a basic web app template using Next.js, Express.js, Mongo, and Docker."
+    />
+    <meta
+      name="keywords"
+      content="React, Reatc.js, Next, Next.js, Express, Express.js, quick, guide, template, docker, frontend, backend, fullstack"
+    />
+    <meta name="author" content="Omri Geda" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/images/favicon/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/images/favicon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/images/favicon/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/images/favicon/site.webmanifest" />
+  </Head><Component {...pageProps} /></>
 }
 
 export default MyApp
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,52 +1,63 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import classes from "../styles/Home.module.css";
+const { mainTitle, title, titleText, divider, code, listCode, caption } = classes;
 
 const Home: NextPage = ({ ENV }: any) => {
+
+  const copyToClipboard = (e: any) => {
+    const { target: { outerText } } = e;
+    { navigator.clipboard.writeText(outerText) }
+    alert("Copied to Clipboard.");
+  }
+
   return (
     <>
-      <Head>
-        <title>Base Next Frontend</title>
-        <meta name="description" content="A skeletal Docker project" />
-      </Head>
-
       <main>
-        <h1 className={classes.titles}>A skeletal Docker\Next.js\Express.js\Mongo project</h1>
+        <div className={mainTitle}>
+          <div className={title}>
+            <h1 className={titleText}>A basic template for a Next.js, Express.js, Mongo, and Docker Web App</h1>
+            <div className={divider} />
+            <h5>Last update: November 19th 2021</h5>
+          </div>
+        </div>
       </main>
 
       <section>
-        <h3 className={classes.titles}>In the command line:</h3>
-        <p>Write &quot;docker-compose up&quot; when in development.</p>
-        <p>Write &quot;ENV=prod docker-compose up&quot; when in production.</p>
-        <p>Current environment variable: {ENV}</p>
+        <div className={title}>
+          <h3 className={titleText}>To run this page in localhost:3000, simply clone or download it from <a target="_blank" href="https://github.com/O-K-G/project" rel="noopener noreferrer">https://github.com/O-K-G/project</a>, and in the command line:</h3>
+          <div className={divider} />
+        </div>
+        <p>Run&nbsp;<button onClick={copyToClipboard} className={code}>docker compose up</button>&nbsp;when in <b>development</b>.</p>
+        <p>Run&nbsp;<button onClick={copyToClipboard} className={code}>ENV=prod docker compose up</button>&nbsp;when in <b>production</b>.</p>
+        <p className={caption}>Current environment variable in this page:&nbsp;<b>{ENV}</b></p>
+        <div className={divider} />
       </section>
 
       <section>
-        <h3 className={classes.titles}>Develop in Ubuntu within Windows:</h3>
-        <p>1. Install Windows features: Windows subsystem for Linux. Virtual Machine Platform.<br></br>
-          2. Update Windows. <br></br>
-          3. Add in Windows' {"Users/<user name/"} the file .wslconfig:<br></br>
-          ---------------<br></br>
+        <h3>Quick Guide - Develop with Ubuntu inside Windows:</h3>
+        <ol>
+          <li>Install the following Windows features: <b>Windows Subsystem for Linux</b> (WSL), <b>Virtual Machine Platform</b>.</li> <a target="_blank" href="https://www.windowscentral.com/how-manage-optional-features-windows-10" rel="noopener noreferrer">https://www.windowscentral.com/how-manage-optional-features-windows-10</a>
+          <li>Update Windows.</li>
+          <li>Add in the folder '<b>{"Users/<user name>/"}</b>' (probably in your main drive) the file '<b>.wslconfig</b>' with the following Text:<br></br>
+            <button onClick={copyToClipboard} className={listCode}>
+              [wsl2]<br></br>
+              memory=8gb<br></br>
+              processors=2<br></br>
 
-          [wsl2]<br></br>
-          memory=8gb<br></br>
-          processors=2<br></br>
-
-          [interop]<br></br>
-          appendWindowsPath = false<br></br>
-
-          --------------- <br></br>
-          4. Install MS store Ubuntu and open it. <br></br>
-          5. Update/upgrade Ubuntu. https://www.cyberciti.biz/faq/upgrade-update-ubuntu-using-terminal/ <br></br>
-          6. Install Node in Linux. https://github.com/nodesource/distributions/blob/master/README.md <br></br>
-          7. Instal Docker + repo. https://docs.docker.com/engine/install/ubuntu/ <br></br>
-          8. Run 'sudo service docker start'<br></br>
-          9. Install Docker Compose V2. https://docs.docker.com/compose/cli-command/#install-on-linux<br></br>
-          10. Manage Docker as a non-root user. https://docs.docker.com/engine/install/linux-postinstall/<br></br>
-          11. Add a bit of code at the end of ~/.bashrc to see the current branch. https://thucnc.medium.com/how-to-show-current-git-branch-with-colors-in-bash-prompt-380d05a24745<br></br>
-          12. Clone a project to a folder within Ubuntu and Docker Compose Up.
-        </p>
-
+              [interop]<br></br>
+              appendWindowsPath = false<br></br>
+            </button>
+          </li>
+          <li>Install the Microsoft Store Ubuntu app and open it.</li>
+          <li>Update/upgrade Ubuntu. <a target="_blank" href="https://www.cyberciti.biz/faq/upgrade-update-ubuntu-using-terminal" rel="noopener noreferrer">https://www.cyberciti.biz/faq/upgrade-update-ubuntu-using-terminal</a></li>
+          <li>Install Node in Linux. <a target="_blank" href="https://github.com/nodesource/distributions/blob/master/README.md" rel="noopener noreferrer">https://github.com/nodesource/distributions/blob/master/README.md</a></li>
+          <li>Instal Docker + repo. <a target="_blank" href="https://docs.docker.com/engine/install/ubuntu" rel="noopener noreferrer">https://docs.docker.com/engine/install/ubuntu</a></li>
+          <li>Run <button onClick={copyToClipboard} className={listCode}>sudo service docker start</button>.</li>
+          <li>Install Docker Compose V2. <a target="_blank" href="https://docs.docker.com/compose/cli-command/#install-on-linux" rel="noopener noreferrer">https://docs.docker.com/compose/cli-command/#install-on-linux</a></li>
+          <li>Manage Docker as a non-root user. <a target="_blank" href="https://docs.docker.com/engine/install/linux-postinstall" rel="noopener noreferrer">https://docs.docker.com/engine/install/linux-postinstall</a></li>
+          <li>Add a bit of code at the end of ~/.bashrc to see the current branch in the terminal. <a target="_blank" href="https://thucnc.medium.com/how-to-show-current-git-branch-with-colors-in-bash-prompt-380d05a24745" rel="noopener noreferrer">https://thucnc.medium.com/how-to-show-current-git-branch-with-colors-in-bash-prompt-380d05a24745</a></li>
+          <li>Clone a project to a folder <b>within Ubuntu</b> and Docker Compose Up</li>
+        </ol>
       </section>
 
       <footer>
